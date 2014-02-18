07c7d98e1dd406838b47d00bc5009358392fbccb
momokan <momokan@chocolapod.sakura.ne.jp>
2014-02-18 18:49:58 +0900

    MappedCharacter の javadoc 整備中

--- src/main/java/net/chocolapod/lwjgfont/MappedCharacter.java
+++ src/main/java/net/chocolapod/lwjgfont/MappedCharacter.java
@@ -23,6 +23,18 @@
  */
 package net.chocolapod.lwjgfont;
 
+/**
+ * The MappedCharacter class represents how each characters of the font are mapped on textures by LWJGFont.
+ * To drawing the character, this class has these informations.
+ * <ul>
+ * <li>a index of the image that the character is mapped onto.</li>
+ * <li>coordinates that the character is mapped on.</li>
+ * <li>a ascent distance from the font's baseline to the top of most alphanumeric characters.</li>
+ * <li>a descent distance from the font's baseline to the bottom of most alphanumeric characters.</li>
+ * <li>a advance distance from the leftmost point to the rightmost point on the string's baseline.</li>
+ * <li>a padding distance around rectangle which the ascent, the descent, and the advance shape</li>
+ * </ul>
+ */
 public class MappedCharacter {
 	private final char		character;
 	private final int		imageIndex;
@@ -60,18 +72,41 @@ public class MappedCharacter {
 		return srcY;
 	}
 
+	/**
+	 * Returns the character's ascent of the font which this MappedCharacter instance is mapped on.<br>
+	 * The font ascent is the distance from the font's baseline to the top of most alphanumeric characters.
+	 * Some characters in the Font might extend above the font ascent line.
+	 * @return the character's ascent of the font
+	 */
 	public int getAscent() {
 		return ascent;
 	}
 
+	/**
+	 * Returns the character's descent of the font which this MappedCharacter instance is mapped on.<br>
+	 * The font descent is the distance from the font's baseline to the bottom of most alphanumeric characters.
+	 * Some characters in the Font might extend below the font descent line.
+	 * @return the character's descent of the font
+	 */
 	public int getDescent() {
 		return descent;
 	}
 
+	/**
+	 * Returns the character's advance of the font which this MappedCharacter instance is mapped on.<br>
+	 * The advance is the distance from the leftmost point to the rightmost point on the string's baseline.
+	 * The advance of any String is the sum of the advances of its characters.
+	 * @return the character's advance of the font
+	 */
 	public int getAdvance() {
 		return advance;
 	}
 
+	/**
+	 * Returns the character's padding distance around rectangle which the ascent, the descent, and the advance shape.<br>
+	 * Some characters might extend over ascent, descent, advance. So each characters are mapped with paddings around it.
+	 * @return the character's padding distance.
+	 */
 	public int getPadding() {
 		return padding;
 	}
