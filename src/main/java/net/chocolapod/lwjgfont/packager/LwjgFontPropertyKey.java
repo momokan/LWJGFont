26c0cd3db1043993178b673417b6e4e5a8b4d80a
momokan <momokan@chocolapod.sakura.ne.jp>
2014-03-05 19:04:41 +0900

    javadoc の整備

--- src/main/java/net/chocolapod/lwjgfont/packager/LwjgFontPropertyKey.java
+++ src/main/java/net/chocolapod/lwjgfont/packager/LwjgFontPropertyKey.java
@@ -30,15 +30,81 @@ import static net.chocolapod.lwjgfont.packager.FontMapPainter.DEFAULT_WRITE_IMAG
 import static net.chocolapod.lwjgfont.packager.LwjgFontFactory.DEFAULT_TEMP_DIR;
 import static net.chocolapod.lwjgfont.packager.LwjgFontFactory.DEFAULT_DIST_DIR;
 
+/**
+ * A LwjgFontPropertyKey represents property keys of LWJGFont configuration properties file (it is lwjgfont.properties at default).<br>
+ * As property key name, each enum'name are converted to be lower case and be separated by periods instead of underscore. 
+ */
 public enum LwjgFontPropertyKey {
-	IMAGE_DRAW(DEFAULT_WRITE_IMAGE),
-	IMAGE_DRAW_FRAME(DEFAULT_WRITE_IMAGE_FRAME),
-	IMAGE_CHARACTER_PADDING(DEFAULT_PADDING),
-	CHARACTER_FILE_DIR(DEFAULT_CHARACTERS_DIR),
-	TEMP_DIR(DEFAULT_TEMP_DIR),
-	DIST_DIR(DEFAULT_DIST_DIR),
+	/**
+	 *	Indicates the artifact name which LWJGFont generates.
+	 * This value is set in a pom.xml to manage this jar as a Maven artifact.<br>
+	 *	Default value is "myfont".<br>
+	 * "artifact.name" is a key of this property on properties file.
+	 */
 	ARTIFACT_NAME("myfont"),
+
+	/**
+	 *	Indicates the artifact version which LWJGFont generates.
+	 * This value is set in a pom.xml to manage this jar as a Maven artifact.<br>
+	 *	Default value is "1.0-SNAPSHOT".<br>
+	 * "artifact.version" is a key of this property on properties file.
+	 */
 	ARTIFACT_VERSION("1.0-SNAPSHOT"),
+
+	/**
+	 * Indicates the directory path which contains character files to be able to render.
+	 * LWJGFont draw all characters of these character files on images.<br>
+	 * Default value is "characters"<br> 
+	 * "character.file.dir" is a key of this property on properties file.
+	 */
+	CHARACTER_FILE_DIR(DEFAULT_CHARACTERS_DIR),
+
+	/**
+	 * Indicates the directory path which LWJGFont generate a jar file on.
+	 * Default value is "". This means current directory.<br> 
+	 * "dist.dir" is a key of this property on properties file.
+	 */
+	DIST_DIR(DEFAULT_DIST_DIR),
+
+	/**
+	 * Indicates the directory path which contains temporary files.
+	 * To generate jar, LWJGFont create some images, java source files, java class files, and other resource files into this directory.<br>
+	 * Notice that when LWJGFont start packaging process, This directory is deleted.<br> 
+	 * Default value is "temp"<br> 
+	 * "temp.dir" is a key of this property on properties file.
+	 */
+	TEMP_DIR(DEFAULT_TEMP_DIR),
+
+	/**
+	 * Indicates padding size around each characters on images.
+	 * If some characters overlap with neighbor characters, specify padding size to more larger.<br>
+	 * Default value is 5.<br> 
+	 * "image.character.padding" is a key of this property on properties file.
+	 */
+	IMAGE_CHARACTER_PADDING(DEFAULT_PADDING),
+
+	/**
+	 * Indicates whether LWJGFont generates some images which characters are drawn on.
+	 * This property is for debug.<br>
+	 * Default value is true and LWJGFont generates images.<br> 
+	 * "image.draw" is a key of this property on properties file.
+	 */
+	IMAGE_DRAW(DEFAULT_WRITE_IMAGE),
+
+	/**
+	 * Indicates whether LWJGFont draws frame around each characters on images.
+	 * This property is for debug.<br>
+	 * Default value is false and LWJGFont dose not draw frames.<br> 
+	 * "image.draw.frame" is a key of this property on properties file.
+	 */
+	IMAGE_DRAW_FRAME(DEFAULT_WRITE_IMAGE_FRAME),
+	
+	/**
+	 *	Indicates the LWJGFont version.
+	 * This value must be specified by users, so it is automatically overrode by LWJGFont.<br>
+	 *	Default value is "<unknown>", and automatically correct to LWJGFont version.<br>
+	 * "lwjgfont.version" is a key of this property on properties file.
+	 */
 	LWJGFONT_VERSION("<unknown>");
 
 	private final String	defaultValue;
@@ -56,12 +122,26 @@ public enum LwjgFontPropertyKey {
 		this.defaultValue = defaultValue;
 	}
 
+	/**
+	 * Returns the default value of this property.
+	 * @return the default value of this property.
+	 */
 	public String getDefaultValue() {
 		return defaultValue;
 	}
-	
-	public static String toResourceReplacePattern(LwjgFontPropertyKey key) {
-		String[]	tokens = key.name().toLowerCase().split("_");
+
+	/**
+	 * Returns a replacement symbol of this property for built-in resources.
+	 * When LWJGFont packages a jar, LWJGFont replaces symbols in built-in resources to its property's value
+	 * and add built-in resources to the jar.<br>
+	 * Built-in resources are MANIFEST.MF, Maven pom.xml, pom.properties and the others.<br>
+	 * <br>
+	 * For example, with ARTIFACT_VERSION property,
+	 * a line "version=%artifactVersion%" in pom.xml is converted to "version=1.0-SNAPSHOT" at default.
+	 * @return a replacement symbol of this property for built-in resources.
+	 */
+	public String toResourceReplacePattern() {
+		String[]	tokens = name().toLowerCase().split("_");
 		String		pattern = "";
 		
 		for (String token: tokens) {
@@ -74,6 +154,10 @@ public enum LwjgFontPropertyKey {
 		return pattern;
 	}
 
+	/**
+	 * Returns a key name of this property.
+	 * @return a key name of this property.
+	 */
 	@Override
 	public String toString() {
 		String	key = name();
